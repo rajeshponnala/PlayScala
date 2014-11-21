24fda72903653eb0606f25fb7ece9992bb99cfc2
rajeshponnala <rajesh.ponnala@technoidentity.com>
2014-11-22 02:20:55 +0530

    add newProduct Get method;save post method using flash

--- PlayScala/app/controllers/Products.scala
+++ PlayScala/app/controllers/Products.scala
@@ -1,10 +1,11 @@
 package  controllers
 
-import play.api.mvc.{Action, Controller}
+import play.api.mvc.{Flash,Action, Controller}
 import models.Product
 import play.api.data.Form
 import play.api.data.Forms.{mapping,longNumber,nonEmptyText}
 import play.api.i18n.Messages
+
 /** Uncomment the following lines as needed **/
 /**
 import play.api.Play.current
@@ -19,20 +20,47 @@ import play.api.libs.json._
 **/
 
 object Products extends Controller {
-  private val ProductForm:Form[Product]=Form(mapping("ean"->longNumber.verifying("validation.ean.duplicate",Product.findByEan(_).isEmpty),
+  private val productForm:Form[Product]=Form(mapping("ean"->longNumber.verifying("validation.ean.duplicate",Product.findByEan(_).isEmpty),
        "name"->nonEmptyText,
        "description"->nonEmptyText
   	)(Product.apply)(Product.unapply)
 )
 
-  def list = Action{
+  def list = Action{ implicit request =>
   	 Ok(views.html.products.list(Product.findAll))
   }
   
-  def show(ean: Long) = Action{
+  def show(ean: Long) = Action{ implicit request =>
   	   Product.findByEan(ean).map { product =>
         Ok(views.html.products.details(product))
        }.getOrElse(NotFound)
-  }			
+  }	
+
+  def newProduct = Action{ implicit request =>
+    val form=if(request.flash.get("error").isDefined)
+        productForm.bind(request.flash.data)
+    else
+        productForm
+    
+    Ok(views.html.products.editProduct(form))
+
+  }
+
+
+  def save = Action{ implicit request =>
+        val newProductForm = productForm.bindFromRequest()
+
+        newProductForm.fold(
+           hasErrors={ form =>
+              Redirect(routes.Products.newProduct()).flashing(Flash(form.data)+("error"->Messages("validation.errors")))
+           },
+
+           success={ newProduct =>
+              Product.add(newProduct)
+              val message = Messages("products.new.success", newProduct.name)
+              Redirect(routes.Products.show(newProduct.ean)).flashing("success" -> message)
+           }
+        )
+      }		
 
 }
