e23670d9e2cbccbd937f6157c42ed26724e5eb3f
rajeshponnala <rajesh.ponnala@technoidentity>
2014-11-21 17:47:04 +0530

    add form field

--- PlayScala/app/controllers/Products.scala
+++ PlayScala/app/controllers/Products.scala
@@ -2,6 +2,9 @@ package  controllers
 
 import play.api.mvc.{Action, Controller}
 import models.Product
+import play.api.data.Form
+import play.api.data.Forms.{mapping,longNumber,nonEmptyText}
+import play.api.i18n.Messages
 /** Uncomment the following lines as needed **/
 /**
 import play.api.Play.current
@@ -16,6 +19,12 @@ import play.api.libs.json._
 **/
 
 object Products extends Controller {
+  private val ProductForm:Form[Product]=Form(mapping("ean"->longNumber.verifying("validation.ean.duplicate",Product.findByEan(_).isEmpty),
+       "name"->nonEmptyText,
+       "description"->nonEmptyText
+  	)(Product.apply)(Product.unapply)
+)
+
   def list = Action{
   	 Ok(views.html.products.list(Product.findAll))
   }
